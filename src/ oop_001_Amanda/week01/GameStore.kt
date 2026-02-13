9cdc6b4fc3e9181092b5e2942c3f82adb1a5a8a5
Mandalyh <thatmandalyh@gmail.com>
2026-02-13 16:04:37 +0700

    week01: (tugas 2) implement discount logic

--- src/ oop_001_Amanda/week01/GameStore.kt
+++ src/ oop_001_Amanda/week01/GameStore.kt
@@ -2,6 +2,14 @@ package oop_001_Amanda.week01
 
 fun main() {
 
-    val gameTitle = "Shadow force"
+    val gameTitle = "Black Myth: Wukong"
     val price = 750000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 20) / 100
+    else (price * 10) / 100
+
