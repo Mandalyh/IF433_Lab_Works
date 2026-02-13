52f3d58a923412cefded8e6cec30eb5596461fa1
Mandalyh <thatmandalyh@gmail.com>
2026-02-13 22:55:06 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_002_Amanda/week02/Student.kt
+++ src/oop_002_Amanda/week02/Student.kt
@@ -1,7 +1,12 @@
 package oop_002_Amanda.week02
 
-class Student(val name: String, val nim: String, val major: String)
-{
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0
+) {
+
     init {
         println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
     }
