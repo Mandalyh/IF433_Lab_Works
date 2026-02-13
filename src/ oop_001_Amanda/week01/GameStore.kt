a647ae078e89b907ae03bbc67fe8af1f207cb54b
Mandalyh <thatmandalyh@gmail.com>
2026-02-13 16:05:22 +0700

    week01: (tugas 2) implement receipt printing

--- src/ oop_001_Amanda/week01/GameStore.kt
+++ src/ oop_001_Amanda/week01/GameStore.kt
@@ -7,9 +7,21 @@ fun main() {
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        originalPrice = price,
+        finalPrice = finalPrice
+    )
 }
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) (price * 20) / 100
     else (price * 10) / 100
 
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+    println("===== SteamKW by Amanda =====")
+    println("Judul Game : $title")
+    println("Harga Asli : Rp $originalPrice")
+    println("Harga Akhir: Rp $finalPrice")
+}
