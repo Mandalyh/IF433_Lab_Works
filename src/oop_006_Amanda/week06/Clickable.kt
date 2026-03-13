a37c3c72692c7ebe8dd0335d89f965c26d50254e
Mandalyh <thatmandalyh@gmail.com>
2026-03-13 14:58:16 +0700

    week06: create Button class implementing Clickable

--- src/oop_006_Amanda/week06/Clickable.kt
+++ src/oop_006_Amanda/week06/Clickable.kt
@@ -3,4 +3,10 @@ package oop_006_Amanda.week06
 interface Clickable {
     val name: String
     fun click()
+
+    class Button(override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil diklik!")
+        }
+    }
 }
