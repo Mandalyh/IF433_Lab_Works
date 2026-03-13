69226397a184052e2fa50cec4a86582031f0e1a2
Mandalyh <thatmandalyh@gmail.com>
2026-03-13 15:45:49 +0700

    week06: (task) create SmartHomeHub logic

--- src/oop_006_Amanda/week06/task1/SmartHomeHub.kt
+++ src/oop_006_Amanda/week06/task1/SmartHomeHub.kt
@@ -1,4 +1,19 @@
 package oop_006_Amanda.week06.task1
 
 class SmartHomeHub {
-}
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("System: Berhasil menambahkan ${device.name} (ID: ${device.id}) ke hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\nSystem: Menjalankan protokol penghematan energi...")
+        for (device in devices) {
+            // Smart Casting (is)
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
