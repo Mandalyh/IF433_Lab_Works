fff4f8a51c271f0d575cd09a3634e16570d6c327
Mandalyh <thatmandalyh@gmail.com>
2026-03-13 15:39:54 +0700

    week06: (task) create SmartLamp

--- src/oop_006_Amanda/week06/task1/SmartLamp.kt
+++ src/oop_006_Amanda/week06/task1/SmartLamp.kt
@@ -1,4 +1,15 @@
 package oop_006_Amanda.week06.task1
 
-class SmartLamp {
+class SmartLamp(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("[$name] Lampu menyala, memberikan cahaya hangat.")
+    }
+
+    override fun turnOff() {
+        println("[$name] Lampu mati.")
+    }
 }
