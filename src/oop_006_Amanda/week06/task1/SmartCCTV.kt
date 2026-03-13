dbe3f2560a34c66b6f2815e7631238da208cf66a
Mandalyh <thatmandalyh@gmail.com>
2026-03-13 15:41:23 +0700

    week06: (task) create SmartCCTV with compound interfaces

--- src/oop_006_Amanda/week06/task1/SmartCCTV.kt
+++ src/oop_006_Amanda/week06/task1/SmartCCTV.kt
@@ -1,4 +1,21 @@
 package oop_006_Amanda.week06.task1
 
-class SmartCCTV {
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("[$name] Kamera aktif dan lensa terbuka.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("[$name] Kamera nonaktif.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("[$name] Memulai perekaman video...")
+    }
 }
