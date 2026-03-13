82d49f931cf24a3afb79008aeef25a4ffbbe7cd2
Mandalyh <thatmandalyh@gmail.com>
2026-03-13 15:40:32 +0700

    week06: (task) create SmartSpeaker

--- src/oop_006_Amanda/week06/task1/SmartSpeaker.kt
+++ src/oop_006_Amanda/week06/task1/SmartSpeaker.kt
@@ -1,4 +1,19 @@
 package oop_006_Amanda.week06.task1
 
-class SmartSpeaker {
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("[$name] Speaker aktif, menunggu perintah suara...")
+    }
+
+    override fun turnOff() {
+        println("[$name] Speaker nonaktif.")
+    }
+
+    fun playMusic(song: String) {
+        println("[$name] Memutar lagu $song dari Spotify.")
+    }
 }
