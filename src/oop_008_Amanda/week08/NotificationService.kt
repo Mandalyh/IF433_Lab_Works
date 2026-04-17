be1a809415822d4e8a7e10fd6ae8d8306025756d
Mandalyh <thatmandalyh@gmail.com>
2026-04-17 15:25:10 +0700

    week08: implement safe call with let block

--- src/oop_008_Amanda/week08/NotificationService.kt
+++ src/oop_008_Amanda/week08/NotificationService.kt
@@ -1,4 +1,15 @@
 package oop_008_Amanda.week08
 
 class NotificationService {
+    fun sendEmail(EmailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
 }
