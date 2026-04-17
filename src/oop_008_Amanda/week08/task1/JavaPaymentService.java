6cc74694f00fcc8bea783c29f6ff2eae5d66a11a
Mandalyh <thatmandalyh@gmail.com>
2026-04-17 16:12:24 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_008_Amanda/week08/task1/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_008_Amanda.week08.task1;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
