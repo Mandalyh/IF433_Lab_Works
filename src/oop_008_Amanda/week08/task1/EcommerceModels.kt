bbeb5134ce5567b7604832c98a3dd4d338e4ab92
Mandalyh <thatmandalyh@gmail.com>
2026-04-17 16:10:24 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_008_Amanda/week08/task1/EcommerceModels.kt
@@ -0,0 +1,5 @@
+package oop_008_Amanda.week08.task1
+
+sealed class Product
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+data class Clothing(val id: String, val name: String, val size: String) : Product()
