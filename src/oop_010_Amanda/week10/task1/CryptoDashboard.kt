b1acaceb82bef57b19c1365e8cb2b07971b1f9fe
Mandalyh <thatmandalyh@gmail.com>
2026-05-08 15:03:59 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_010_Amanda/week10/task1/CryptoDashboard.kt
+++ src/oop_010_Amanda/week10/task1/CryptoDashboard.kt
@@ -1,17 +1,19 @@
 package oop_010_Amanda.week10.task1
 
 fun main() {
+
     val coinRepo = WalletRepository<Coin>()
 
-    coinRepo.add(Coin("BTC", 0.5))
-    coinRepo.add(Coin("ETH", 2.0))
-    coinRepo.add(Coin("USDT", 1000.0))
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 10.5))
+    coinRepo.add(Coin("USDT", 2500.0))
 
     val response = ApiResponse(
         "200 OK",
         coinRepo.getAll()
     )
 
+    println("===== CRYPTO DASHBOARD =====")
     println("Status Response: ${response.status}")
     println()
 
@@ -21,9 +23,28 @@ fun main() {
         println()
     }
 
+    println("===== SEARCH RESULT =====")
+
+    val searchResult = coinRepo.searchByName("ET")
+
+    searchResult.forEach {
+        println(it.name)
+    }
+
+    println()
+
     val txRepo = WalletRepository<Transaction>()
 
     txRepo.add(Transaction("TX001", 50000.0))
     txRepo.add(Transaction("TX002", 120000.0))
     txRepo.add(Transaction("TX003", 75000.0))
+
+    println("===== TRANSACTIONS =====")
+
+    txRepo.getAll().forEach {
+        println("Transaction ID: ${it.id}")
+        println("Amount: ${it.amount}")
+        println()
+    }
+
 }
