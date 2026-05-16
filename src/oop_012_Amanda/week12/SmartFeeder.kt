35edc7814301ed26d16e5b25453433ef1f6c413f
Mandalyh <thatmandalyh@gmail.com>
2026-05-16 19:28:54 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_012_Amanda/week12/SmartFeeder.kt
+++ src/oop_012_Amanda/week12/SmartFeeder.kt
@@ -21,4 +21,17 @@ fun dispenseKibble(
 
 fun main() {
     var currentKibbleStock = 50
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+    } catch (e: DispenserJamException) {
+        println("Error Dispenser: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Stok: ${e.message}")
+    } catch (e: Exception) {
+        println("Terjadi kesalahan umum: ${e.message}")
+    }
 }
