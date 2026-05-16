5472d76351c1ad14957ed0cb80890172028e0ce0
Mandalyh <thatmandalyh@gmail.com>
2026-05-16 19:29:59 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_012_Amanda/week12/SmartFeeder.kt
+++ src/oop_012_Amanda/week12/SmartFeeder.kt
@@ -33,5 +33,7 @@ fun main() {
         println("Error Stok: ${e.message}")
     } catch (e: Exception) {
         println("Terjadi kesalahan umum: ${e.message}")
+    } finally {
+        println("Siklus pengecekan dispenser pagi selesai.")
     }
 }
