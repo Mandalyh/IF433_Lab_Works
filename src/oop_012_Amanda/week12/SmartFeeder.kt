f51126757dd5f0365cec641c4b406496d9b433e4
Mandalyh <thatmandalyh@gmail.com>
2026-05-16 19:25:41 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_012_Amanda/week12/SmartFeeder.kt
+++ src/oop_012_Amanda/week12/SmartFeeder.kt
@@ -1,4 +1,16 @@
 package oop_012_Amanda.week12
 
-class SmartFeeder {
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    return availableGram - requestedGram
 }
